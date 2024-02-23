62afd1e71a1fd36421abecf260037faad3d73cab
bvlgah <octopus.busts_0w@icloud.com>
2024-02-23 23:22:49 +0800

    Use a worklist algorithm to compute live variables

--- llvm_code_example/src/LiveVariableAnalysis.cpp
+++ llvm_code_example/src/LiveVariableAnalysis.cpp
@@ -1,4 +1,5 @@
 #include "llvm/ADT/PostOrderIterator.h"
+#include "llvm/ADT/SmallPtrSet.h"
 #include "llvm/IR/BasicBlock.h"
 #include "llvm/IR/CFG.h"
 #include "llvm/IR/Function.h"
@@ -10,6 +11,8 @@
 #include <algorithm>
 #include <cassert>
 #include <iterator>
+#include <queue>
+#include <tuple>
 #include <unordered_map>
 #include <unordered_set>
 #include <utility>
@@ -22,14 +25,13 @@ namespace {
 class Analyzer {
 public:
   explicit Analyzer(const Function &F)
-      : Fun(F), Blocks(), LiveIns(), LiveOuts(), Kills(), Gens() {
-    Blocks.reserve(Fun.size());
-    ReversePostOrderTraversal<const Function *> RPO(&Fun);
-    std::copy(RPO.begin(), RPO.end(), std::back_inserter(Blocks));
+      : Fun(F), Blocks(), LiveIns(), LiveOuts(), Defs(), Uses() {
+    initVisitingOrder();
   }
 
   void run() {
-    computeLiveInOut();
+    init();
+    compute();
     print();
   }
 
@@ -37,21 +39,58 @@ private:
   using ValueSetT = std::unordered_set<const Value *>;
   using BlockValueMapT = std::unordered_map<const BasicBlock *, ValueSetT>;
 
+  void initVisitingOrder() {
+    Blocks.reserve(Fun.size());
+    SmallPtrSet<const BasicBlock *, 8> VisitedBB;
+    for (const BasicBlock &BB : Fun) {
+      if (!succ_empty(&BB))
+        continue;
+      std::copy(ipo_ext_begin<const BasicBlock*>(&BB, VisitedBB),
+                ipo_ext_end<const BasicBlock *>(&BB, VisitedBB),
+                std::back_inserter(Blocks));
+    }
+    assert(Blocks.size() == Fun.size() && "some basic blocks are not visited");
+  }
+
+  void init() {
+    for (const BasicBlock *Block : Blocks) {
+      LiveIns.emplace(Block, ValueSetT());
+      LiveOuts.emplace(Block, ValueSetT());
+      BlockValueMapT::iterator DefIt;
+      BlockValueMapT::iterator UseIt;
+      std::tie(DefIt, std::ignore) = Defs.emplace(Block, ValueSetT());
+      std::tie(UseIt, std::ignore) = Uses.emplace(Block, ValueSetT());
+
+      for (const Instruction &Inst : *Block) {
+        for (const Use &U : Inst.operands()) {
+          const Value *V = U.get();
+          if (llvm::isa<llvm::Constant>(V)
+              || V->getType()->isLabelTy()
+              || DefIt->second.count(V))
+            continue;
+          UseIt->second.insert(V);
+        }
+        if (!Inst.getType()->isVoidTy() && !UseIt->second.count(&Inst))
+          DefIt->second.insert(&Inst);
+      }
+    }
+  }
+
   void print() const {
     llvm::errs() << "Result of live variable analyses for function '"
                  << Fun.getName() << "'\n";
-    for (const BasicBlock *Block : Blocks) {
+    for (const BasicBlock &Block : Fun) {
       llvm::errs() << "  live-in variables of block '"
-                   << Block->getName() << "': ";
-      auto LiveInIt = LiveIns.find(Block);
+                   << Block.getName() << "': ";
+      auto LiveInIt = LiveIns.find(&Block);
       assert(LiveInIt != LiveIns.end() && "an unknown basic block");
       printValueSet(llvm::errs(), LiveInIt->second);
       llvm::errs() << "\n";
 
-      auto LiveOutIt = LiveOuts.find(Block);
+      auto LiveOutIt = LiveOuts.find(&Block);
       assert(LiveOutIt != LiveOuts.end() && "an unknown basic block");
       llvm::errs() << "  live-out variables of block '"
-                   << Block->getName() << "': ";
+                   << Block.getName() << "': ";
       printValueSet(llvm::errs(), LiveOutIt->second);
       llvm::errs() << "\n";
     }
@@ -72,30 +111,34 @@ private:
     Stream << "}";
   }
 
-  void computeLiveInOut() {
-    for (const BasicBlock *Block : Blocks) {
-      auto &Kill= Kills[Block];
-      auto &Gen = Gens[Block];
-      for (const Instruction &Inst : *Block) {
-        for (const Use &U : Inst.operands()) {
-          const Value *V = U.get();
-          if (llvm::isa<llvm::Constant>(V)
-              || V->getType()->getTypeID() == llvm::Type::LabelTyID)
-            continue;
-          if (!Kill.count(V))
-            Gen.insert(V);
-        }
-        Kill.insert(&Inst);
-      }
-    }
+  void compute() {
+    /* bool Changed = true; */
+    /* while (Changed) { */
+    /*   Changed = false; */
+    /*   for (auto It = Blocks.rbegin(), End = Blocks.rend(); */
+    /*        It != End; ++It) { */
+    /*     updateLiveOut(*It); */
+    /*     Changed = updateLiveIn(*It) || Changed; */
+    /*   } */
+    /* } */
 
-    bool Changed = true;
-    while (Changed) {
-      Changed = false;
-      for (auto BlockIt = Blocks.rbegin();
-           BlockIt != Blocks.rend(); ++BlockIt) {
-        Changed = updateLiveIn(*BlockIt) || Changed;
-        Changed = updateLiveOut(*BlockIt) || Changed;
+    std::queue<const BasicBlock *> WorkList;
+    for (auto It = Blocks.rbegin(), End = Blocks.rend();
+         It != End; ++It)
+      WorkList.push(*It);
+    SmallPtrSet<const BasicBlock *, 8> QueuedBB(Blocks.begin(), Blocks.end());
+    while (!WorkList.empty()) {
+      const BasicBlock *BB = WorkList.front();
+      WorkList.pop();
+      QueuedBB.erase(BB);
+      updateLiveOut(BB);
+      if (updateLiveIn(BB)) {
+        for (const BasicBlock *Pred : predecessors(BB)) {
+          if (QueuedBB.count(Pred))
+            continue;
+          WorkList.push(Pred);
+          QueuedBB.insert(Pred);
+        }
       }
     }
   }
@@ -103,21 +146,20 @@ private:
   bool updateLiveIn(const BasicBlock *Block) {
     auto &LiveIn = LiveIns[Block];
     const auto &LiveOut = LiveOuts[Block];
-    const auto &Gen = Gens[Block];
-    const auto &Kill = Kills[Block];
+    const auto &Use = Uses[Block];
+    const auto &Def = Defs[Block];
     size_t OldLiveInSize = LiveIn.size();
     for (const Value *V : LiveOut) {
-      if (!Kill.count(V))
+      if (!Def.count(V))
         LiveIn.insert(V);
     }
-    for (const Value *V : Gen)
+    for (const Value *V : Use)
       LiveIn.insert(V);
     return LiveIn.size() > OldLiveInSize;
   }
 
-  bool updateLiveOut(const BasicBlock *Block) {
+  void updateLiveOut(const BasicBlock *Block) {
     auto &LiveOut = LiveOuts[Block];
-    size_t OldLiveOutSize = LiveOut.size();
     for (auto SuccIt = llvm::succ_begin(Block); SuccIt != llvm::succ_end(Block);
         ++SuccIt) {
       assert(LiveIns.count(*SuccIt) && "an unknown successive basic block");
@@ -125,15 +167,14 @@ private:
         LiveOut.insert(V);
       }
     }
-    return LiveOut.size() > OldLiveOutSize;
   }
 
   const llvm::Function &Fun;
   std::vector<const BasicBlock *> Blocks;
   BlockValueMapT LiveIns;
   BlockValueMapT LiveOuts;
-  BlockValueMapT Kills;
-  BlockValueMapT Gens;
+  BlockValueMapT Defs;
+  BlockValueMapT Uses;
 };
 
 class LiveVariableAnalysis : public FunctionPass {
